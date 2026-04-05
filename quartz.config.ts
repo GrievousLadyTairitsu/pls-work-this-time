57c4bdb4c7146867c025a066958a1c56c86120f0
Daniel Liu <danielliu@Daniels-Mac-mini.local>
2026-04-04 20:53:32 -0400

    Quartz sync: Apr 4, 2026, 8:53 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://grievousladytairitsu.github.io/pls-work-this-time/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
